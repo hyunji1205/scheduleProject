30de809a7b609ebf670a81bb13d64251dd7f267f
hyunji1205 <sguswl1205@gmail.com>
2024-04-20 22:47:07 +0900

    피패키지 분리중

---
+++ src/main/java/org/example/dto/Schedule.java
@@ -0,0 +1,11 @@
+package org.example.dto;
+
+public class Schedule {
+    public String date;
+    public String todo;
+
+    public Schedule(String date, String todo) {
+        this.date = date;
+        this.todo = todo;
+    }
+}
